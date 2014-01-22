e8e786dce9c120a558e251713a6c8ac5eaa3c4ca
HarleyKwyn <kwyn.meagher@gmail.com>
2014-01-22 15:15:54 -0800

    Added ended property to SongModel.js

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -4,6 +4,10 @@ var SongModel = Backbone.Model.extend({
   play: function(){
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+
+  ended: function(){
+    this.trigger('ended', this);
   }
 
 });
